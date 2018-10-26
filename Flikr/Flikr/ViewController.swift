b68837291c19cf89e4dc21e4f2a807bc9455117e
John Pope <jp@fieldstormapp.com>
2018-10-26 12:24:15 -0400

    begin.

---
+++ Flikr/Flikr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flikr
+//
+//  Created by John Pope on 10/26/18.
+//  Copyright © 2018 John Pope. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
